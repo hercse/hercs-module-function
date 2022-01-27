dd6f989cd4148d242b32f4affe536d58a15b40ad
HERCS <github@hercs.art>
2022-01-27 08:18:05 +0800

    更新滑鼠端口

--- README.md
+++ README.md
@@ -1,11 +1,20 @@
 # hercs-module-function
 自己用的模組化功能
 
-# 模組化CSS (完整)
+## 模組化CSS (完整)
 若要使用，請輸入以下代碼：
 
 ```
 <link rel="stylesheet" href="https://module-function.herc.se/scss/style.css">
+```
+
+## 滑鼠功能
+
+```
+cursor((e) => {
+            // function here, when mouse moveing.
+            console.log(e);
+        }, "body"); // defult give css variable to "body".
 ```
 
 會不定時更新，若無法使用請記得重新到此網站查閱。
---
+++ js/cursor.js
@@ -0,0 +1,17 @@
+function cursor(code, element) {
+
+    window.addEventListener('mousemove', (event) => {
+        const cursor = {
+            "X": event.clientX,
+            "Y": event.clientY,
+        }
+        if (element) {
+            // console.log(`指派給：${element}`);
+            var element_style = document.querySelector(element).style;
+            element_style.setProperty("--x", cursor.X + "px");
+            element_style.setProperty("--y", cursor.Y + "px");
+        }
+        code(cursor);
+    });
+
+}
